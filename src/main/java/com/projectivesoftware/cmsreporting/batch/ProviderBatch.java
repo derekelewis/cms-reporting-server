b894a46934111d08a8bfe98ce5ec8e4448f3011e
Derek Lewis <derek_lewis@projectivesoftware.com>
2018-02-15 07:42:08 -0600

    Change providerReader to utilize HibernateCursorItemReader

--- src/main/java/com/projectivesoftware/cmsreporting/batch/ProviderBatch.java
+++ src/main/java/com/projectivesoftware/cmsreporting/batch/ProviderBatch.java
@@ -7,31 +7,24 @@
 package com.projectivesoftware.cmsreporting.batch;
 
 import com.projectivesoftware.cmsreporting.domain.Provider;
-import com.projectivesoftware.cmsreporting.service.ProviderRepository;
 import org.castor.core.util.Assert;
-import org.springframework.batch.item.data.RepositoryItemReader;
+import org.hibernate.SessionFactory;
+import org.springframework.batch.item.database.HibernateCursorItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 public class ProviderBatch {
 
     @Bean
-    public RepositoryItemReader<Provider> providerReader(ProviderRepository providerRepository) {
-        Assert.notNull(providerRepository, "providerRepository must not be null!");
-        RepositoryItemReader<Provider> reader = new RepositoryItemReader<>();
-        reader.setRepository(providerRepository);
-        reader.setMethodName("findAll");
-        reader.setPageSize(1000);
-
-        Map<String, Sort.Direction> sortMap = new HashMap<>();
-        sortMap.put("npi", Sort.Direction.ASC);
-
-        reader.setSort(sortMap);
+    public HibernateCursorItemReader<Provider> providerReader(EntityManagerFactory entityManagerFactory) {
+        Assert.notNull(entityManagerFactory, "entityManagerFactory must not be null!");
+        HibernateCursorItemReader<Provider> reader = new HibernateCursorItemReader<>();
+        reader.setSessionFactory(entityManagerFactory.unwrap(SessionFactory.class));
+        reader.setQueryString("from Provider");
+        reader.setFetchSize(1000);
 
         try {
             reader.afterPropertiesSet();
